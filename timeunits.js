ce59fe0c4b2bbd6cae21aa2d228e6726efaf842e
danielkalen <d@danielkalen.com>
2016-10-14 20:24:19 -0400

    can now be imported either as a static file or as a commonJS module

--- timeunits.js
+++ timeunits.js
@@ -1,10 +1,20 @@
-module.exports = {
-	'millisecond': 1,
-	'second': 1000,
-	'minute': 60000,
-	'hour': 3600000,
-	'day': 24 * 3600000,
-	'week': 7 * 24 * 3600000,
-	'month': 28 * 24 * 3600000,
-	'year': 364 * 24 * 3600000
-}
+(function(){
+	var timeUnits = {
+		'millisecond': 1,
+		'second': 1000,
+		'minute': 60000,
+		'hour': 3600000,
+		'day': 24 * 3600000,
+		'week': 7 * 24 * 3600000,
+		'month': 28 * 24 * 3600000,
+		'year': 364 * 24 * 3600000
+	};
+
+	if (typeof module === 'object') {
+		module.exports = timeUnits;
+	} else if (typeof window === 'object') {
+		window.timeUnits = timeUnits;
+	}
+
+	return timeUnits;
+})();
